f4609c5e6a98a2af6b0c2193a487b2bbf4fac770
Dima Ishkinin <mac_98@bk.ru>
2024-05-26 11:46:40 +0300

    Version 0.2

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
-import random, sys
+import random
+import sys
 
-print ('Камень, ножницы, бумага')
+print('Камень, ножницы, бумага')
 
 # В этих переменных накапливается количество
 # побед, поражений и ничьих
@@ -8,57 +9,57 @@ wins = 0
 losses = 0
 ties = 0
 
-while True: # главный цикл игры
-    print ('%s побед, %s поражений, %s ничьих' % (wins, losses, ties))
-    while True: # цикл выбора хода
+while True:  # главный цикл игры
+    print('%s побед, %s поражений, %s ничьих' % (wins, losses, ties))
+
+    while True:  # цикл выбора хода
         print('Выберите ход: (к)амень, (н)ожницы,(б)умага или (в)ыход')
         playerMove = input()
         if playerMove == 'в':
-            sys.exit() # выход из программы
+            sys.exit()  # выход из программы
         if playerMove == 'к' or playerMove == 'н' or playerMove == 'б':
-            break # выход из цикла выбора хода
-        print ('Введите "к", "н", "б" или "в".')
-        
-#Отображение выбора пользователя
-if playerMove == 'к':
-    print ('Камень и ...')
-elif playerMove == 'н':
-    print ('Ножницы и ...')
-elif playerMove == 'б':
-    print ('Бумага и ...')
-    
-#Отображение выбора компьютера
+            break  # выход из цикла выбора хода
+        print('Введите "к", "н", "б" или "в".')
 
-randomNumber = random.randint(1, 3)
-if randomNumber == 1:
-    computerMove = 'к'
-    print ('Камень')
-elif randomNumber == 2:
-    computerMove = 'н'
-    print ('Ножницы')
-elif randomNumber == 3:
-    computerMove = 'б'
-    print ('Бумага')
-    
-#Отображение и учет результата
-if playerMove == computerMove:
-    print ('Ничья!')
-    ties = ties + 1
-elif playerMove == 'к' and computerMove == 'н':
-    print ('Вы выиграли!')
-    wins = wins + 1
-elif playerMove == 'б' and computerMove == 'к':
-    print ('Вы выиграли!')
-    wins = wins + 1
-elif playerMove == 'н' and computerMove == 'б':
-    print ('Вы выиграли!')
-    wins = wins + 1
-elif playerMove == 'к' and computerMove == 'б':
-    print ('Вы проиграли')
-    losses = losses + 1
-elif playerMove == 'б' and computerMove == 'н':
-    print ('Вы проиграли')
-    losses = losses + 1
-elif playerMove == 'н' and computerMove == 'к':
-    print ('Вы проиграли')
-    losses = losses + 1
+    # Отображение выбора пользователя
+    if playerMove == 'к':
+        print('Камень и ...')
+    elif playerMove == 'н':
+        print('Ножницы и ...')
+    elif playerMove == 'б':
+        print('Бумага и ...')
+
+    # Отображение выбора компьютера
+    randomNumber = random.randint(1, 4)
+    if randomNumber == 1:
+        computerMove = 'к'
+        print('Камень')
+    elif randomNumber == 2:
+        computerMove = 'н'
+        print('Ножницы')
+    elif randomNumber == 3:
+        computerMove = 'б'
+        print('Бумага')
+
+    # Отображение и учет результата
+    if playerMove == computerMove:
+        print('Ничья!')
+        ties = ties + 1
+    elif playerMove == 'к' and computerMove == 'н':
+        print('Вы выиграли!')
+        wins = wins + 1
+    elif playerMove == 'б' and computerMove == 'к':
+        print('Вы выиграли!')
+        wins = wins + 1
+    elif playerMove == 'н' and computerMove == 'б':
+        print('Вы выиграли!')
+        wins = wins + 1
+    elif playerMove == 'к' and computerMove == 'б':
+        print('Вы проиграли')
+        losses = losses + 1
+    elif playerMove == 'б' and computerMove == 'н':
+        print('Вы проиграли')
+        losses = losses + 1
+    elif playerMove == 'н' and computerMove == 'к':
+        print('Вы проиграли')
+        losses = losses + 1
